d080e844e7d9369ab86c3cc4f879dac30efaca26
Yasser <yasserdalouzi@gmail.com>
2024-10-02 23:47:17 +0100

    Update fbConfig.js

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -6,13 +6,6 @@ import 'firebase/storage'
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDYridcQndkkVH-6yxmpYknduKN-GlI8gI",
-  authDomain: "rent-car118.firebaseapp.com",
-  databaseURL: "https://rent-car118.firebaseio.com",
-  projectId: "rent-car118",
-  storageBucket: "rent-car118.appspot.com",
-  messagingSenderId: "685208685659",
-  appId: "1:685208685659:web:00f5e3c5c9d4793b"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({
